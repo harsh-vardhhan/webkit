d9d1b9734c2f73b71e6f9c0356d2653caecdc283
trakbit <harsh.vardhan.611@gmail.com>
2020-12-15 18:09:34 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,13 @@ module.exports = {
                             wrapperStyle: 'margin-bottom: 1.0725rem'
                         }
                     },
+                    {
+                        resolve: 'gatsby-remark-embed-video-lite',
+                        options: {
+                            width: 300,
+                            height: 150
+                        }
+                    },
                     'gatsby-remark-prismjs',
                     'gatsby-remark-copy-linked-files',
                     'gatsby-remark-smartypants'
